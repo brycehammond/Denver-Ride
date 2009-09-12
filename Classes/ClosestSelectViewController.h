a4d936d275b6fdc43896df58e2418ca4185f2252
Bryce <bryce@semanticchickens.com>
2009-09-12 13:47:35 +0000

    Add artwork and fix time issue

---
+++ Classes/ClosestSelectViewController.h
@@ -0,0 +1,16 @@
+//
+//  ClosestSelectViewController.h
+//  RTD
+//
+//  Created by bryce.hammond on 9/12/09.
+//  Copyright 2009 Wall Street On Demand, Inc.. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ClosestSelectViewController : UIViewController {
+
+}
+
+@end
--- Classes/Stop.m
+++ Classes/Stop.m
@@ -25,11 +25,19 @@
 {
 	NSString *amOrPm = @"A";
 	int hours = [[self timeInMinutes] intValue] / 60;
-	if(hours > 12)
+	if(hours > 24)
+	{
+		hours -= 24;
+	}
+	else if(hours > 12)
 	{
 		hours -= 12;
 		amOrPm = @"P";
-	}	
+	}
+	else if(hours == 12)
+	{
+		amOrPm = @"P";
+	}
 	int minutes = [[self timeInMinutes] intValue] % 60;
 	NSString *formattedTime = (minutes < 10) ? [NSString stringWithFormat:@"%i:0%i%@",hours,minutes,amOrPm] : [NSString stringWithFormat:@"%i:%i%@",hours,minutes,amOrPm];
 	
