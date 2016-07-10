dc70b8c3fc0b4f334adefdd585bf9f069fd7870d
Bryce Hammond <bryce@flyreel.co>
2016-07-10 15:41:16 -0600

    Update bcycle scraping script

--- bcycle_scrap.rb
+++ bcycle_scrap.rb
@@ -15,12 +15,12 @@ docks = Array.new
 
 current_dock = nil
 
-open("https://denver.bcycle.com/home.aspx").each do |line|
-  if line =~ /var point = new google.maps.LatLng\((.*?), (.*?)\)\;/ then
+open("https://denver.bcycle.com/").each do |line|
+  if line =~ /var point = new google.maps.LatLng\((.*?), (.*?)\)\;/
     current_dock = Dock.new
     current_dock.lattitude = $1
     current_dock.longitude = $2
-  elsif line =~ /var marker = new createMarker\(point, "<div class='location'><strong>(.*?)<\/strong>.*?<\/div><div class='avail'>Bikes available: <strong>(\d+)<\/strong><br \/>Docks available: <strong>(\d+)<\/strong>.*?", icon, back\)\;/ then
+  elsif line =~ /var marker = new createMarker\(point, "<div class='markerTitle'><h3>(.*?)<\/h3><\/div>.*?<div class='markerAvail'>.*?<h3>(.*?)<\/h3>.*?<h3>(.*?)<\/h3>Docks.*?"/
     current_dock.name = $1
     current_dock.bikes_available = $2
     current_dock.docks_available = $3
